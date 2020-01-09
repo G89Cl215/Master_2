eef2d422a16c43116184c3071efa0191ca5eb58d
G89Cl215 <thomas.gwed@gmail.com>
2020-01-09 15:30:56 +0100

    Feat(Rho_Pollard):
    Factorisation of big composite numbers without using memory to find collisions.

--- M2_crypto_assymetrique/TP4/Rho_Pollard_facto_sansmem.c
+++ M2_crypto_assymetrique/TP4/Rho_Pollard_facto_sansmem.c
@@ -18,10 +18,29 @@ void	ft_pseudo_aleatoire(mpz_t x, const mpz_t to_factor)
 
 void	ft_Rho_Pollard_factorsansmem(mpz_t factor, const mpz_t to_factor)
 {
-	mpz_t	x1, x2;
+	int		k;
+	mpz_t	x1, x2, gcd, sub;
 
-	mpz_inits(x1, x2, NULL);
-	mpz_set_ui(x, 1);
+	k = 0;
+	mpz_inits(x1, x2, gcd, sub, NULL);
+	mpz_set_ui(x1, 1);
+	mpz_set_ui(x2, 1);
+	while (mpz_cmp(to_factor, x1) > 0)
+	{
+		ft_pseudo_aleatoire(x1, to_factor);
+		mpz_sub(sub, x1, x2);
+		mpz_gcd(gcd, sub, to_factor);
+		mpz_gcd(gcd, gcd, to_factor);
+		if (mpz_cmp_ui(gcd, 1) > 0)
+		{
+			mpz_set(factor, gcd);
+			mpz_clears(x1, x2, gcd, sub, NULL);
+			return ;
+		}
+		k++;
+		if (!(k & (k - 1)))
+			mpz_set(x2, x1);
+	}
 }
 
 int		main(int ac, char **av)
@@ -36,9 +55,9 @@ int		main(int ac, char **av)
 	}
 	mpz_inits(to_factor, factor, NULL);
 	mpz_set_str(to_factor, av[1], 10);
-	ft_Rho_Pollard_factorsansmem(factor, to_factor);
 	printf("Factor: trying to factor: ");
 	mpz_out_str(stdout, 10, to_factor);
+	ft_Rho_Pollard_factorsansmem(factor, to_factor);
 	printf("\nFactor: we found the factor: ");
 	mpz_out_str(stdout, 10, factor);
 	printf("\n");
